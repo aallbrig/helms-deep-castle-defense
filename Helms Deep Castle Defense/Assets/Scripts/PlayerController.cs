0c9323cbbfc864f0e12a6db73d337282646a896a
Andrew Allbright <aallbrig@gmail.com>
2021-01-22 09:17:32 -0500

    Player can move mage character around helms deep in testing ground

---
+++ Helms Deep Castle Defense/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Transform _transform;
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _transform = transform;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            var movementVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            _agent.destination = _transform.position + movementVector;
+        }
+    }
+}
